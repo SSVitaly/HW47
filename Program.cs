e742f0af0ef5e9dabab30e6fc28999f39bd7e23e
SVitaly <user7_490@mail.ru>
2023-02-02 21:15:29 +0300

    добавили решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]:f1} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(-100, 101) / 10.0;
+        }
+    }
+}
+
+Console.WriteLine("Создаю двумерный массив вещественных чисел 3х4:");
+double[,] array = new double[3, 4];
+PrintArray(array);
+Console.WriteLine();
+
+Console.WriteLine("Заполняю массив случайными числами в диапазоне от -10 до 10:");
+FillArray(array);
+PrintArray(array);
